2ba5aabee4a1291ef2905de37a7ae97899976e4c
lixd <dandan580826@163.com>
2018-09-01 11:58:40 +0800

    “周考完成

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,22 @@ var minCss = require('gulp-clean-css');
 
 var uglify = require('gulp-uglify');
 
+var server = require('gulp-webserver');
+
+//gulp-babel  ES6 ---> ES5  babel-preset-es2015必须下载
+
+var babel = require('gulp-babel');
+
+var url = require('url');
+
+var fs = require('fs');
+
+var path = require('path');
+
+var swiperJson = require('./mock/swiper.json');
+
+var searchJson = require('./mock/search.json');
+
 //编译scss 压缩css
 gulp.task('devCss', function() {
     return gulp.src('./src/scss/*.scss')
@@ -23,11 +39,54 @@ gulp.task('watch', function() {
 //压缩js
 gulp.task('minJs', function() {
     return gulp.src(['./src/js/**/*.js', '!./src/js/libs/*.js'])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
         .pipe(uglify())
         .pipe(gulp.dest('build'))
 })
 
+//起服务
+
+gulp.task('devserver', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 9090, //配置端口号
+            middleware: function(req, res, next) { //拦截前端请求
+                var pathname = url.parse(req.url).pathname;
+
+                if (pathname === '/favicon.ico') {
+                    res.end('');
+                    return
+                }
+
+                console.log(pathname)
+
+                if (pathname === '/api/swiper') {
+                    res.end(JSON.stringify({ code: 1, data: swiperJson }))
+                } else if (pathname === '/api/search') {
+                    var key = url.parse(req.url, true).query.key;
+                    var target = [];
+                    //复 match
+                    searchJson.forEach(function(item) {
+                        if (item.title.match(key)) {
+                            target.push(item);
+                        }
+                    })
+
+                    res.end(JSON.stringify({ code: 1, data: target }))
+                } else {
+                    pathname = pathname === '/' ? '/index.html' : pathname;
+
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
+                }
+
+
+
+            }
+        }))
+})
 
 //开发环境
 
-gulp.task('dev', gulp.series('devCss', 'minJs', 'watch'))
+gulp.task('dev', gulp.series('devCss', 'devserver', 'minJs', 'watch'))
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,57 @@
-var a = 10;
+$(function() {
 
-var b = 10;
+
+    $.ajax({
+        url: '/api/swiper',
+        dataType: 'json',
+        success: function(res) {
+            console.log(res);
+            if (res.code === 1) {
+                var str = '';
+
+                res.data.forEach(function(item) {
+                    str += `
+                    <div class="swiper-slide">
+                        <img src="${item.url}" alt="">
+                    </div>
+                        `;
+                })
+                $('.swiper-wrapper').html(str);
+                new Swiper('.swiper-container');
+            }
+        },
+        error: function(error) {
+            console.warn(error);
+        }
+    })
+
+
+    $('#ipt').on('input', function() {
+        var val = $(this).val();
+
+        if (!val) {
+            $('.result').html('');
+            return
+        } else {
+            $.ajax({
+                url: '/api/search?key=' + val,
+                dataType: 'json',
+                success: function(res) {
+                    console.log(res);
+                    if (res.code === 1) {
+                        var liStr = '';
+
+                        res.data.forEach(function(item) {
+                            liStr += ` <li>${item.title}</li>`
+                        })
+
+                        $('.result').html(liStr);
+                    }
+                },
+                error: function(error) {
+                    console.warn(error)
+                }
+            })
+        }
+    })
+})
